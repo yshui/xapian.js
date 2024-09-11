18d3c96772f6e4cf4ef6830044f40374f8c85551
Builder <builder@example.org>
2024-09-11 06:09:04 +0100

    bump to xapian-core 1.4.26, and only build wasm

--- run.sh
+++ run.sh
@@ -1,56 +1,30 @@
 #! /bin/bash
 
 # skip version="1.4.17"
-version="1.4.16"
+version="1.4.26"
 name="xapian-core-$version"
 exports='["_prepare", "_commit", "_key", "_add", "_clean", "_query", "_percent", "_languages", "_snippet", "_compact", "_release"]'
 
-wget https://oligarchy.co.uk/xapian/$version/$name.tar.xz
+[ -e $name.tar.xz ] || wget https://oligarchy.co.uk/xapian/$version/$name.tar.xz
 tar -xf $name.tar.xz
 
-# make libxapian.a
-pushd $name
-emconfigure ./configure CPPFLAGS='-DFLINTLOCK_USE_FLOCK' CXXFLAGS='-Oz -s USE_ZLIB=1' --disable-backend-remote
-emmake make
-popd
+if [ ! -f ./$name/.libs/libxapian.so ]; then
+  # make libxapian.a
+  pushd $name
+  emconfigure ./configure CPPFLAGS='-DFLINTLOCK_USE_FLOCK' CXXFLAGS='-Oz -s USE_ZLIB=1' --disable-backend-remote
+  emmake make
+  popd
+fi
 
 FLAGS=(-Oz \
   -s ALLOW_MEMORY_GROWTH=1
   -s USE_ZLIB=1 \
-  -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' \
+  -s EXPORTED_RUNTIME_METHODS='["cwrap"]' \
   -s EXPORTED_FUNCTIONS='["_prepare", "_commit", "_key", "_add", "_clean", "_query", "_percent", "_languages", "_snippet", "_compact", "_release"]' \
   -std=c++11 \
   -I./$name \
   -I./$name/include \
   -I./$name/common)
-
-# with exceptions and no wasm
-em++ "${FLAGS[@]}" \
-  -s NO_DYNAMIC_EXECUTION=1 \
-  -s WASM=0 \
-  -s DISABLE_EXCEPTION_CATCHING=0 \
-  xapian.cc ./$name/.libs/libxapian.so -o xapian_exception_nowasm.js
-
-# with exceptions and no wasm with indexedDB support
-em++ "${FLAGS[@]}" \
-  -s NO_DYNAMIC_EXECUTION=1 \
-  -s WASM=0 \
-  -s DISABLE_EXCEPTION_CATCHING=0 \
-  -lidbfs.js \
-  xapian.cc ./$name/.libs/libxapian.so -o xapian_exception_nowasm_db.js
-
-# without exceptions and no wasm
-em++ "${FLAGS[@]}" \
-  -s NO_DYNAMIC_EXECUTION=1 \
-  -s WASM=0 \
-  xapian.cc ./$name/.libs/libxapian.so -o xapian_noexception_nowasm.js
-
-# without exceptions and no wasm with indexedDB support
-em++ "${FLAGS[@]}" \
-  -s NO_DYNAMIC_EXECUTION=1 \
-  -s WASM=0 \
-  -lidbfs.js \
-  xapian.cc ./$name/.libs/libxapian.so -o xapian_noexception_nowasm_db.js
 
 # with exceptions and wasm
 em++ "${FLAGS[@]}" \
